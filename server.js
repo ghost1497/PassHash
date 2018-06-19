ecf77436908b5aded7f3323e411e5f2988693f87
Rodriguez <frodrig6@cscinfo.com>
2018-06-19 14:04:55 -0400

    added config files v10

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/PasswordHashProgram'));
+app.use(express.static('./dist/PasswordHashProgram'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join('/dist/PasswordHashProgram/index.html'));
+    res.sendFile(path.join('./dist/PasswordHashProgram/index.html'));
 });
 
